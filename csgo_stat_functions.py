aaa228c16e2d979c20c8f90cbfe28dc62d58bbb0
R S <rspet123@gmail.com>
2022-02-09 23:08:06 -0500

    Add files via upload
    Preparing to add threading to the demo parsing function, allows us to dramatically speed up the processing of new demos.

--- csgo_stat_functions.py
+++ csgo_stat_functions.py
@@ -1,5 +1,5 @@
 #A Handful of useful functions for calculating stats based on round frames and kill data
-
+from csgo.parser import DemoParser
     
 def matchKillToFrame(kill: dict, match_round: dict):
     frame_after_kill = None
@@ -18,7 +18,7 @@ def matchKillToFrame(kill: dict, match_round: dict):
     return 0
     
     
-def calculateKillAdvantage(kill: dict, match_round: dict):
+def calculateKillAdvantage(kill: dict, match_round: dict, debug = False):
     #frame_after = matchKillToFrame(kill,match_round)[1]
     try:
         frame_before = matchKillToFrame(kill,match_round)[0]
@@ -30,6 +30,19 @@ def calculateKillAdvantage(kill: dict, match_round: dict):
     enemy_players_before_kill = frame_before[kill['victimSide'].lower()]['alivePlayers']
     #enemy_players_after_kill = frame_after[kill['victimSide'].lower()]['alivePlayers']
     
+        
     advantage = friendly_players_before_kill-enemy_players_before_kill
+    if debug:
+        #TODO
+        #Seems a bit messed up
+        if advantage < 0:
+            print(kill['attackerTeam'] + " are down " + str(abs(advantage)))
+        elif advantage == 0:
+            print("teams are even")
+        else:
+            print(kill['attackerTeam'] + " is up " + str(abs(advantage)))
     return advantage
     
+
+def thread_process_demo(demo_path: str,demos_dict: dict):
+    demos_dict.append(DemoParser(demofile="resources\\" + demo_path, demo_id=demo_path,outpath="JSONLogs", parse_rate=128, parse_frames=True).parse())
